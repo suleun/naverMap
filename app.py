03c910cf4bef948f352415868d7086a7dc5cb36c
soeun <teaaa030@gmail.com>
2021-09-26 21:51:38 +0900

    Create app.py
    server

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+client = MongoClient('IP주소', 27017, username="", password="")
+db = client.dbsparta_plus_week3
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
